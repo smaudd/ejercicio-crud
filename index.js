5a23019d8df4423c671033e15ed342db69a6bcac
Santiago Marcano <smarcanoweb@gmail.com>
2022-07-01 09:34:14 -0300

    Practice

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000
 
 app.use(express.static('public'))
 
+// CRUD = Create Read Update Delete
 // GET -> lectura
 // POST -> escritura
 // PUT -> actualizar
@@ -22,44 +23,22 @@ app.use(express.static('public'))
 
 app.get('/todo', (request, response) => {
   // Lee el fichero JSON y da como respuesta sus contenidos
-  return response.json(JSON.parse(fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf8')))
 })
 
 app.get('/todo/:index', (request, response) => {
   // Lee el fichero todos.json y da como respuesta el objeto Todo en el index especificado por parámetros
-  const index = request.params.index
-  const file = fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf8')
-  const json = JSON.parse(file)
-  return response.json(json[index])
 })
 
 app.post('/todo', (request, response) => {
   // Lee el body de la petición y escribe el nuevo Todo en el fichero todos.json
-  const file = fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf8')
-  const json = JSON.parse(file)
-  json.push(request.body)
-  fs.writeFileSync(path.join(__dirname, 'todos.json'), JSON.stringify(json))
-  return response.json({ message: "Created" })
 })
 
 app.delete('/todo/:index', (request, response) => {
   // Borra del fichero todos.json el objeto Todo en el index especificado por parámetros
-  const index = request.params.index
-  const file = fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf8')
-  let json = JSON.parse(file)
-  json = json.slice(index, 1)
-  fs.writeFileSync(path.join(__dirname, 'todos.json'), JSON.stringify(json))
-  return response.json({ message: "Deleted" })
 })
 
 app.put('/todo/:index', (request, response) => {
   // Modifica del fichero todos.json el objeto Todo en el index especificado por parámetros
-  const index = request.params.index
-  const file = fs.readFileSync(path.join(__dirname, 'todos.json'), 'utf8')
-  const json = JSON.parse(file)
-  json[index] = request.body
-  fs.writeFileSync(path.join(__dirname, 'todos.json'), JSON.stringify(json))
-  return response.json({ message: "Updated" })
 })
 
 app.listen(port, () => {
